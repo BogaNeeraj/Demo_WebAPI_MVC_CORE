ea83f629236fa160dbddf9684714031f0bbb4121
BogaNeeraj <92246820+BogaNeeraj@users.noreply.github.com>
2021-10-30 21:58:59 +0530

    Add files via upload

---
+++ EmpContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Demo_WebAPI_MVC_CORE.Models;
+
+namespace Demo_WebAPI_MVC_CORE
+{
+    public class EmpContext : DbContext
+    {
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Neeraj.Boga\source\repos\DemoWebApplication1\App_Data\MyDatabase1.mdf;Integrated Security=True");
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
